eee9ba914933aa44894a68165e0cdded7fe391e8
usr/bin/poobinus <129819902+FernsReuben@users.noreply.github.com>
2023-12-06 18:35:33 -0500

    Update order_page.php

--- order_page.php
+++ order_page.php
@@ -1,3 +1,18 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+require_once "config.php";
+
+$current_username = '';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -82,7 +97,12 @@ function getPrizes($conn) {
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            $prizes[] = $row;
+            // Select specific attributes from the row
+            $prizes[] = [
+                'Prize_ID' => $row['Prize_ID'],
+                'name' => $row['name'],
+                'cost' => $row['cost'],
+            ];
         }
         return $prizes;
     } else {
