e5394a00d02a2441a58e796746c66b08edbacef5
usr/bin/poobinus <129819902+FernsReuben@users.noreply.github.com>
2023-12-06 22:03:11 -0500

    Update order_page.php

--- order_page.php
+++ order_page.php
@@ -130,19 +130,26 @@ echo '<button onclick="showPopup(\'Place Order\')">Place Order</button>';
 echo '<button onclick="showPopup(\'Cancel Order\')">Cancel Order</button>';
 echo '</div>';
 
+echo '<form action="process_order.php" method="post">'; // Assuming you will process the order in process_order.php
+
 echo '<table>';
-echo '<tr><th>Prize ID</th><th>Name</th><th>Cost</th></tr>';
+echo '<tr><th>Prize ID</th><th>Name</th><th>Cost</th><th>Order Quantity</th></tr>';
 
 foreach ($prizes as $prize) {
-    // Display prize details
+    // Display prize details with checkboxes
     echo '<tr>';
     echo '<td>' . $prize['Prize_ID'] . '</td>';
     echo '<td>' . $prize['name'] . '</td>';
     echo '<td>' . $prize['cost'] . '</td>';
+    echo '<td><input type="checkbox" name="prizes[]" value="' . $prize['Prize_ID'] . '"> Quantity <input type="number" name="quantity[' . $prize['Prize_ID'] . ']" min="1" value="1"></td>';
     echo '</tr>';
 }
 
 echo '</table>';
+
+echo '<input type="submit" value="Submit Order">';
+
+echo '</form>';
 
 // Close the database connection
 $conn->close();
