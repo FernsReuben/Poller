be2bc897eed8807838d920a8c14accb169ba563c
usr/bin/poobinus <129819902+FernsReuben@users.noreply.github.com>
2023-12-06 23:23:41 -0500

    Update order_page.php

--- order_page.php
+++ order_page.php
@@ -92,13 +92,14 @@ if ($conn->connect_error) {
 // Function to fetch data from the prizes table
 function getPrizes($conn)
 {
-    $sql = "SELECT * FROM prizes";
+    $sql = "SELECT * FROM Prizes";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             // Select specific attributes from the row
             $prizes[] = [
+                'Prize_ID' => $row['Prize_ID'],
                 'name' => $row['name'],
                 'cost' => $row['cost'],
             ];
@@ -108,6 +109,24 @@ function getPrizes($conn)
         echo "Error executing query: " . $conn->error;
         return [];
     }
+}
+
+// Function to get the user's current credits
+function getUserCredits($conn, $username)
+{
+    $credits = 0;
+    $stmt = $conn->prepare("SELECT credits FROM User WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $credits = $row['credits'];
+    }
+
+    $stmt->close();
+    return $credits;
 }
 
 // Read data from the database (using only the prizes table)
@@ -126,11 +145,67 @@ echo '<h1>Order Page</h1>';
 
 // Header buttons
 echo '<div>';
-echo '<button onclick="showPopup(\'Place Order\')">Place Order</button>';
+echo '<button onclick="placeOrder()">Place Order</button>';
 echo '<button onclick="showPopup(\'Cancel Order\')">Cancel Order</button>';
 echo '</div>';
 
-echo '<form action="process_order.php" method="post">'; // Assuming you will process the order in process_order.php
+// Process order form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["prizes"])) {
+    // Get the user ID
+    $current_username = $_SESSION["username"];
+    $user_id_query = "SELECT User_ID FROM User WHERE username = ?";
+    $user_id_stmt = $conn->prepare($user_id_query);
+    $user_id_stmt->bind_param("s", $current_username);
+    $user_id_stmt->execute();
+    $user_id_result = $user_id_stmt->get_result();
+    $user_id_row = $user_id_result->fetch_assoc();
+    $user_id = $user_id_row["User_ID"];
+
+    // Get the user's current credits
+    $current_credits = getUserCredits($conn, $current_username);
+
+    // Calculate the total cost of selected prizes
+    $total_cost = 0;
+    foreach ($_POST["prizes"] as $prize_id) {
+        foreach ($prizes as $prize) {
+            if ($prize["Prize_ID"] == $prize_id) {
+                $total_cost += $prize["cost"];
+                break;
+            }
+        }
+    }
+
+    // Check if the user has sufficient credits
+    if ($current_credits >= $total_cost) {
+        // Subtract the cost from the user's credits
+        $new_credits = $current_credits - $total_cost;
+        $update_credits_query = "UPDATE User SET credits = ? WHERE User_ID = ?";
+        $update_credits_stmt = $conn->prepare($update_credits_query);
+        $update_credits_stmt->bind_param("ii", $new_credits, $user_id);
+        $update_credits_stmt->execute();
+
+        // Insert orders into the Orders database
+        $insert_order_query = "INSERT INTO Orders (User_ID, Prize_ID) VALUES (?, ?)";
+        $insert_order_stmt = $conn->prepare($insert_order_query);
+
+        foreach ($_POST["prizes"] as $prize_id) {
+            $insert_order_stmt->bind_param("ii", $user_id, $prize_id);
+            $insert_order_stmt->execute();
+        }
+
+        // Close the prepared statements
+        $update_credits_stmt->close();
+        $insert_order_stmt->close();
+    } else {
+        // User doesn't have sufficient credits, display an error message or handle it accordingly
+        echo '<script>alert("Insufficient credits to place the order.");</script>';
+    }
+
+    // Close the user ID statement
+    $user_id_stmt->close();
+}
+
+echo '<form action="order_page.php" method="post">'; // Assuming you will process the order on the same page
 
 echo '<table>';
 echo '<tr><th>Name</th><th>Cost</th><th>Select</th></tr>';
@@ -140,7 +215,7 @@ foreach ($prizes as $prize) {
     echo '<tr>';
     echo '<td>' . $prize['name'] . '</td>';
     echo '<td>' . $prize['cost'] . '</td>';
-    echo '<td><input type="checkbox" name="prizes[]" value="' . $prize['name'] . '"> Quantity <input type="number" name="quantity[' . $prize['name'] . ']" min="1" value="1"></td>';
+    echo '<td><input type="checkbox" name="prizes[]" value="' . $prize['Prize_ID'] . '"></td>';
     echo '</tr>';
 }
 
@@ -170,7 +245,11 @@ $conn->close();
             document.body.removeChild(popup);
         }, 2000);
     }
-</script>
 
+    function placeOrder() {
+        // Submit the form to process the order
+        document.querySelector("form").submit();
+    }
+</script>
 </body>
 </html>
